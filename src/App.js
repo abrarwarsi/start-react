46b0ba05617d6c12987a3430b88da2c40de2e9b8
abrarwarsi <35664410+abrarwarsi@users.noreply.github.com>
2020-02-24 19:00:05 +0530

    todo-app phase2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./styles.css";
+import TodoItem from "./TodoItem";
 
 function App() {
   const styles = {
@@ -10,18 +11,12 @@ function App() {
   return (
     <div>
       <h2 style={styles}>To-Do List</h2>
-      <input type="checkbox" />
-      <span>Some kind of task 1</span>
-      <br />
-      <input type="checkbox" />
-      <span>Some kind of task 2</span>
-      <br />
-      <input type="checkbox" />
-      <span>Some kind of task 3</span>
-      <br />
-      <input type="checkbox" />
-      <span>Some kind of task 4</span>
-      <br />
+      <div className="todo-list">
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+      </div>
     </div>
   );
 }
---
+++ src/TodoItem.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function TodoItem() {
+  return (
+    <div className="todo-item">
+      <input type="checkbox" />
+      <p>Some kind of task</p>
+    </div>
+  );
+}
+
+export default TodoItem;
