dfc4c0b8b588c0c49e70754144e2031521fc03f8
abrarwarsi <35664410+abrarwarsi@users.noreply.github.com>
2020-03-03 18:57:28 +0530

    todo-app phase 4

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-import React from "react";
+import React, { Component } from "react";
 import "./styles.css";
 import TodoItem from "./TodoItem";
 import todosData from "./todosData";
 
-function App() {
-  const listItems = todosData.map(item => (
-    <TodoItem key={item.id} item={item} />
-  ));
-  return (
-    <div>
-      <h2>To-Do List</h2>
-      <div className="todo-list">{listItems}</div>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: todosData
+    };
+  }
+  render() {
+    const listItems = this.state.todos.map(item => (
+      <TodoItem key={item.id} item={item} />
+    ));
+    return (
+      <div>
+        <h2>To-Do List</h2>
+        <div className="todo-list">{listItems}</div>
+      </div>
+    );
+  }
 }
 
 export default App;
